1b6a7111a292ac2088a6ba693e2a56d0ce12463e
Michcioperz <michcioperz@gmail.com>
2013-07-13 20:53:48 +0000

    First imports

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.title = "Brokenet Arcade"
+    t.author = "Rebel Team All Hail Noah"
+    t.version = "0.8.0"
+    t.screen.width = 800
+    t.screen.height = 600
+    t.screen.fullscreen = false
+    t.modules.joystick = false
+    t.modules.physics = false
+end
---
+++ port.lua
@@ -0,0 +1,1 @@
+Particle = require "lib.finnticle"
